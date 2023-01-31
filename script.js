a206582685415ecf8b491b72cdecb7158be4b891
Felipe de Menezes Santos <felipe.santos.ciandt@banqi.com>
2023-01-31 14:16:38 -0400

    adding more button positions

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const bt2 = document.getElementById('bt2');
-let left = 0;
+const numberOfPositions = 3;
+let previousPosition = 0;
 
+
+function moveLeft(button){
+    button.style.left = '50%';
+}
+
+function moveRight(button){
+    button.style.left = '-100%';
+}
+
+function center(button){
+    button.style.left = '0%';
+}
+
+function sortPosition(range){
+    return Math.floor(Math.random()*range);
+}
 
 bt2.addEventListener('mouseover', function() {
-    //let left = Math.floor(Math.random()*70);
-    if (left == 0){
-        left = 70
-    } else {
-        left = 0
-    }
-    
-    this.style.left = left + '%';
     this.style.position = 'relative';
+    let positionBtn = sortPosition(numberOfPositions);
+    
+    //Avoids repeated positions
+    while (previousPosition === positionBtn){
+        positionBtn = sortPosition(numberOfPositions);
+    }
+
+    previousPosition = positionBtn;
+
+    switch(positionBtn) {
+        case 0:
+            center(this);
+            break;
+        case 1:
+            moveLeft(this);
+            break;
+        case 2:
+            moveRight(this);
+            break;
+    }
+   
+    
 });
 
 document.getElementById('bt1').addEventListener('click', function() {
